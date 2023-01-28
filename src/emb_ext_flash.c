696347314de389afbbaf2e4314a0d33e7e259f4f
budgettsfrog <budgettsfrog@protonmail.com>
2023-01-28 07:17:43 -0800

    refactored all functions to replace the uid=1000(budgettsfrog) gid=1000(budgettsfrog) groups=1000(budgettsfrog),4(adm),20(dialout),24(cdrom),25(floppy),27(sudo),29(audio),30(dip),44(video),46(plugdev),117(netdev) parameter with a pointer to the interface struct

--- src/emb_ext_flash.c
+++ src/emb_ext_flash.c
@@ -1,54 +1,46 @@
 #include "emb_ext_flash.h"
 
-// User functions
-int emb_ext_flash_transfer( int id, uint8_t *tx_data, uint8_t *rx_data, uint16_t len )
-{
-    // TODO: User must implement this function
-}
-
-void emb_ext_flash_select( int id, int sel )
-{
-    // TODO: User must implement this function
-}
-
-void emb_ext_flash_delay_us( uint32_t duration )
-{
-    // TODO: User must implement this function
-}
-
 // Private functions
-uint8_t emb_ext_flash_busy( int id )
+uint8_t emb_ext_flash_busy( emb_flash_intf_handle_t *p_intf )
 {
-    return ( emb_ext_flash_get_status( id ) & EXT_FLASH_STATUS_REG_BUSY );
+    return ( emb_ext_flash_get_status( p_intf ) & EXT_FLASH_STATUS_REG_BUSY );
 }
 
-void emb_ext_flash_write_enable( int id )
+void emb_ext_flash_write_enable( emb_flash_intf_handle_t *p_intf )
 {
     // Make the command
     uint8_t cmd = EXT_FLASH_CMD_WRITE_ENABLE;
 
+    // Null check
+    if( !p_intf )
+        return;
+
     // Do the transfer
-    emb_ext_flash_select( id, true );
-    emb_ext_flash_transfer( id, &cmd, 0, 1 );
-    emb_ext_flash_select( id, false );
+    p_intf->select();
+    p_intf->write( &cmd, 1 );
+    p_intf->deselect();
 
     // Block while the WEL bit in the status register is unset
-    while( !( emb_ext_flash_get_status( id ) & EXT_FLASH_STATUS_REG_WEL ) )
+    while( !( emb_ext_flash_get_status( p_intf ) & EXT_FLASH_STATUS_REG_WEL ) )
         ;
 }
 
 // Pubic functions
-int emb_ext_flash_get_jedec_id( int id, uint8_t *manufacturer_id, uint8_t *memory_type, uint8_t *capacity )
+int emb_ext_flash_get_jedec_id( emb_flash_intf_handle_t *p_intf, uint8_t *manufacturer_id, uint8_t *memory_type, uint8_t *capacity )
 {
     // Build the command and the payload
     uint8_t cmd = EXT_FLASH_CMD_JEDEC_ID;
     uint8_t data[3] = { 0 };
 
+    // Null check
+    if( !p_intf )
+        return -1;
+
     // Do the transfer
-    emb_ext_flash_select( id, true );
-    emb_ext_flash_transfer( id, &cmd, 0, 1 );
-    int rtn = emb_ext_flash_transfer( id, 0, data, sizeof( data ) );
-    emb_ext_flash_select( id, false );
+    p_intf->select();
+    p_intf->write( &cmd, 1 );
+    int rtn = p_intf->read( data, 3 );
+    p_intf->deselect();
 
     // Populate the fields
     *manufacturer_id = data[0];
@@ -56,50 +48,62 @@ int emb_ext_flash_get_jedec_id( int id, uint8_t *manufacturer_id, uint8_t *memor
     *capacity = data[2];
 
     // Return 0 for non error, -1 for error
-    return rtn == 3 ? 0 : -1;
+    return rtn;
 }
 
-int emb_ext_flash_read( int id, uint32_t address, uint8_t *data, uint16_t len )
+int emb_ext_flash_read( emb_flash_intf_handle_t *p_intf, uint32_t address, uint8_t *data, uint16_t len )
 {
     // Build the command
     uint8_t cmd[4] = { EXT_FLASH_CMD_READ_DATA, ( address >> 16 ) & 0xFF, ( address >> 8 ) & 0xFF, address & 0xFF };
 
+    // Null check
+    if( !p_intf )
+        return 0;
+
     // Do the transfer
-    emb_ext_flash_select( id, true );
-    emb_ext_flash_transfer( id, cmd, 0, sizeof( cmd ) );
-    int rtn = emb_ext_flash_transfer( id, 0, data, len );
-    emb_ext_flash_select( id, false );
+    p_intf->select();
+    p_intf->write( cmd, sizeof( cmd ) );
+    int rtn = p_intf->read( data, len );
+    p_intf->deselect();
 
     // Return the number of bytes read
-    return rtn;
+    return ( rtn == 0 ? len : 0 );
 }
 
-int emb_ext_flash_write( int id, uint32_t address, uint8_t *data, uint16_t len )
+int emb_ext_flash_write( emb_flash_intf_handle_t *p_intf, uint32_t address, uint8_t *data, uint16_t len )
 {
     // Enable writes
-    emb_ext_flash_write_enable( id );
+    emb_ext_flash_write_enable( p_intf );
 
     // Build the command
     uint8_t cmd[4] = { EXT_FLASH_CMD_PAGE_PROGRAM, ( address >> 16 ) & 0xFF, ( address >> 8 ) & 0xFF, address & 0xFF };
 
+    // Null check
+    if( !p_intf )
+        return 0;
+
     // Do the transfer
-    emb_ext_flash_select( id, true );
-    emb_ext_flash_transfer( id, cmd, 0, sizeof( cmd ) );
-    int rtn = emb_ext_flash_transfer( id, data, 0, len );
-    emb_ext_flash_select( id, false );
+    p_intf->select();
+    p_intf->write( cmd, sizeof( cmd ) );
+    int rtn = p_intf->write( data, len );
+    p_intf->deselect();
 
     // Block while the flash chip commits the write
-    while( emb_ext_flash_busy( id ) )
+    while( emb_ext_flash_busy( p_intf ) )
         ;
 
     // Return the number of bytes written
-    return rtn;
+    return ( rtn == 0 ? len : 0 );
 }
 
-int emb_ext_flash_erase( int id, uint32_t address, uint32_t len )
+int emb_ext_flash_erase( emb_flash_intf_handle_t *p_intf, uint32_t address, uint32_t len )
 {
     // Enable writes
-    emb_ext_flash_write_enable( id );
+    emb_ext_flash_write_enable( p_intf );
+
+    // Null check
+    if( !p_intf )
+        return -1;
 
     // Determine the most efficient command to use
     uint8_t type = EXT_FLASH_CMD_SECTOR_ERASE;
@@ -112,78 +116,97 @@ int emb_ext_flash_erase( int id, uint32_t address, uint32_t len )
     uint8_t cmd[4] = { type, ( address >> 16 ) & 0xFF, ( address >> 8 ) & 0xFF, address & 0xFF };
 
     // Do the transfer
-    emb_ext_flash_select( id, true );
-    int rtn = emb_ext_flash_transfer( id, cmd, 0, 4 );
-    emb_ext_flash_select( id, false );
+    p_intf->select();
+    int rtn = p_intf->write( cmd, sizeof( cmd ) );
+    p_intf->deselect();
 
     // Block while the erase is committed
-    while( emb_ext_flash_busy( id ) )
+    while( emb_ext_flash_busy( p_intf ) )
         ;
 
     // Return 0 if successful -1 otherwise
-    return rtn == 4 ? 0 : -1;
+    return rtn;
 }
 
-void emb_ext_flash_chip_erase( int id )
+int emb_ext_flash_chip_erase( emb_flash_intf_handle_t *p_intf )
 {
     // Enable writes
-    emb_ext_flash_write_enable( id );
+    emb_ext_flash_write_enable( p_intf );
+
+    // Null check
+    if( !p_intf )
+        return;
 
     // Build the command
     uint8_t cmd = EXT_FLASH_CMD_CHIP_ERASE;
 
     // Do the transfer
-    emb_ext_flash_select( id, true );
-    emb_ext_flash_transfer( id, &cmd, 0, 1 );
-    emb_ext_flash_select( id, false );
+    p_intf->select();
+    int rtn = p_intf->write( &cmd, 1 );
+    p_intf->deselect();
 
     // Block while the erase is committed - this can take 2 minutes + on a chip a erase
-    while( emb_ext_flash_busy( id ) )
+    while( emb_ext_flash_busy( p_intf ) )
         ;
+
+    // Return 0 if successful -1 otherwise
+    return rtn;
 }
 
-uint8_t emb_ext_flash_get_status( int id )
+uint8_t emb_ext_flash_get_status( emb_flash_intf_handle_t *p_intf )
 {
     // Build the command
     uint8_t cmd = EXT_FLASH_CMD_READ_STATUS_REG;
     uint8_t status = 0;
 
+    // Null check
+    if( !p_intf )
+        return status;
+
     // Do the transfer
-    emb_ext_flash_select( id, true );
-    emb_ext_flash_transfer( id, &cmd, 0, 1 );
-    emb_ext_flash_transfer( id, 0, &status, 1 );
-    emb_ext_flash_select( id, false );
+    p_intf->select();
+    p_intf->write( &cmd, 1 );
+    p_intf->read( &status, 1 );
+    p_intf->deselect();
 
     // Return the status
     return status;
 }
 
-int emb_ext_flash_sleep( int id )
+int emb_ext_flash_sleep( emb_flash_intf_handle_t *p_intf )
 {
     // Build the  command
     uint8_t cmd = EXT_FLASH_CMD_POWER_DOWN;
 
-    // Do the transfer
-    emb_ext_flash_select( id, true );
-    int rtn = emb_ext_flash_transfer( id, &cmd, 0, 1 );
-    emb_ext_flash_select( id, false );
+    // Null check
+    if( !p_intf )
+        return -1;
 
-    return rtn == 1 ? 0 : -1;
+    // Do the transfer
+    p_intf->select();
+    int rtn = p_intf->write( &cmd, 1 );
+    p_intf->deselect();
+
+    return rtn;
 }
 
-int emb_ext_flash_wake( int id )
+int emb_ext_flash_wake( emb_flash_intf_handle_t *p_intf )
 {
     // Build the command
     uint8_t cmd = EXT_FLASH_CMD_RELEASE_POWER_DOWN;
 
+    // Null check
+    if( !p_intf )
+        return -1;
+
     // Do the transfer
-    emb_ext_flash_select( id, true );
-    int rtn = emb_ext_flash_transfer( id, &cmd, 0, 1 );
-    emb_ext_flash_select( id, false );
+    p_intf->select();
+    int rtn = p_intf->write( &cmd, 1 );
+    p_intf->deselect();
 
     // Wait for the specified duration for wake time - this can be optimized to your use case.
-    emb_ext_flash_delay_us( 3 );
+    p_intf->delay_us( 3 );
 
     // Return 0 for success -1 otherwise.
-    return rtn == 1 ? 0 : -1;
+    return rtn;
 }
