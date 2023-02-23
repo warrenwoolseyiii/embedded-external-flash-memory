7fc20fcf0e3683d206070493ee8defe35456105c
budgettsfrog <budgettsfrog@protonmail.com>
2023-02-22 16:33:27 -0800

    fix the getversion script

--- utilities/version/get_version.sh
+++ utilities/version/get_version.sh
@@ -4,9 +4,9 @@
 input_file="utilities/version/version.json"
 
 # Parse the JSON file to extract the constants
-major=$(cat $input_file | jq -r '.VERSION_MAJOR')
-minor=$(cat $input_file | jq -r '.VERSION_MINOR')
-rev=$(cat $input_file | jq -r '.VERSION_REV')
+major=$(cat $input_file | jq -r '.EMBEDDED_FLASH_MAJOR')
+minor=$(cat $input_file | jq -r '.EMBEDDED_FLASH_MINOR')
+rev=$(cat $input_file | jq -r '.EMBEDDED_FLASH_REV')
 
 # Echo the version string
 echo "$major.$minor.$rev"
